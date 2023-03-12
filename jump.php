0ebce9596b5dab1199c14462e2dc71fdba542692
Austix <98461851+Austixu@users.noreply.github.com>
2023-03-12 21:18:55 +0800

    Add files via upload

---
+++ jump.php
@@ -0,0 +1,16 @@
+﻿<?php
+$con = mysqli_connect("localhost","user","password","database");
+if (!$con)
+{
+    die('Could not connect: ' . mysqli_error($con));
+}
+$arg = $_GET['code'];
+mysqli_set_charset($con, "utf8");
+$sql="SELECT * FROM `linkData` WHERE linkNum = ".str_replace("c","",$arg);
+$result = mysqli_query($con,$sql);
+$row = mysqli_fetch_array($result);
+mysqli_close($con);
+$url = $row['linkUrl'];
+header("Location:http://".$url);
+exit;
+?>
---
+++ mysql.php
@@ -0,0 +1,19 @@
+﻿<?php
+$con = mysqli_connect("localhost","s1622997","12345678","s1622997");
+if (!$con)
+{
+    die('Could not connect: ' . mysqli_error($con));
+}
+$arg = $_GET['url'];
+mysqli_set_charset($con, "utf8");
+$sql="SELECT `Num` FROM `lastNum` WHERE 1";
+$result = mysqli_query($con,$sql);
+$num = mysqli_fetch_array($result);
+$num2 = $num[0]+1;
+$sql="UPDATE lastNum SET Num = Num + 1";
+$result = mysqli_query($con,$sql);
+$sql="INSERT INTO linkData (linkNum,linkUrl) VALUES (".$num2.",\"".$arg."\")";
+$result = mysqli_query($con,$sql);
+mysqli_close($con);
+echo "<p>生成成功！地址：xxx.com/".$num2."</p>";
+?>
